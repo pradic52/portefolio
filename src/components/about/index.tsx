e454511fe9b8ddc1f374806a9cc8046a2f479b4e
pradic52 <tokpa2003@gmail.com>
2025-09-10 21:02:25 +0200

    Update index.tsx

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,9 +1,7 @@
-import { useTranslation } from "react-i18next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../assets/animate.css";
 
 export default function About() {
-  const { t } = useTranslation();
 
   return (
     <section id="about" className="bg-white py-5 min-vh-100 d-flex align-items-center">
@@ -12,9 +10,13 @@ export default function About() {
           <div className="col-lg-8 text-center">
             <h2 className="display-5 fw-bold mb-4">À propos de moi</h2>
             <p className="lead text-muted">
-              {t(
-                "I am Tokpa Noël Doré, a passionate and dedicated IT support apprentice. With a strong foundation in computer science and a keen interest in technology, I am committed to providing exceptional technical support and solutions. My goal is to enhance user experiences and ensure smooth operations through effective problem-solving and communication skills."
-              )}
+              Je suis étudiant en DEP en soutien informatique à Montréal. Après avoir obtenu mon 
+              baccalauréat scientifique en Guinée et commencé des études en NTIC à l'Université Gamal
+              Abdel Nasser, j'ai choisi de poursuivre mon parcours au Québec pour développer mes compétences
+              techniques et profissionnelles. <br />
+              Curieux et motivé, je combine mon expérience en service à la clientèle avec ma passion poursuivre
+              l'informatique. Je recherche actuellement un stage en soutien TI afin d'appliquer mes connaissances
+              et acquérir de l'expérience concrète sur le terrain.
             </p>
           </div>
         </div>
@@ -22,3 +24,4 @@ export default function About() {
     </section>
   );
 }
+
