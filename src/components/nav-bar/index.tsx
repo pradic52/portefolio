11ea9b4d69b0317ce01571bf7e0e25c5c8aecb4e
pradic52 <tokpa2003@gmail.com>
2025-09-10 21:02:53 +0200

    Update index.tsx

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,38 +1,37 @@
 import Nav from "react-bootstrap/Nav";
-import { useTranslation } from "react-i18next";
 
 function MainNav() {
-  const { t } = useTranslation();
-
+  const persoStyle:string = "text-dark text-uppercase"
   return (
     <Nav
-      className="justify-content-center fixed-top bg-light shadow"
-      defaultActiveKey="/home"
-      style={{ scrollBehavior: "smooth" }}
+      className="justify-content-center d-flex align-items-center fixed-top shadow bg-dark-subtle"
+      defaultActiveKey={"/home"}
+      variant="underline"
+      style={{ scrollBehavior: "smooth", height:60}}
     >
       <Nav.Item>
-        <Nav.Link eventKey="/home" href="#home">
-          {t("Home")}
+        <Nav.Link eventKey="/home" href="#home" className={persoStyle}>
+          Acceuil
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/about" href="#about">
-          {t("About")}
+        <Nav.Link eventKey="/about" href="#about" className={persoStyle}>
+          A propos
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/skills" href="#skills">
-          {t("Skills")}
+        <Nav.Link eventKey="/skills" href="#skills" className={persoStyle}>
+          Compétences
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/projects" href="#projects">
-          {t("Projects")}
+        <Nav.Link eventKey="/projects" href="#projects" className={persoStyle}>
+          Projets
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/contact" href="#contact">
-          {t("Contact")}
+        <Nav.Link eventKey="/contact" href="#contact" className={persoStyle}>
+          Contactes
         </Nav.Link>
       </Nav.Item>
     </Nav>
@@ -40,3 +39,4 @@ function MainNav() {
 }
 
 export default MainNav;
+
